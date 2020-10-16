baa71e0a70b3b259865ef3aacd71049442152d9f
awarmath <andrew@carmunity.io>
2020-10-16 14:12:18 -0500

    add enemy robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,32 +3,40 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+console.log(enemyNames.length);
+for(var i = 0; i < enemyNames.length; i++) {
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index");
+}
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators!");
 
-    var promptFight = window.prompt("Would you like to Fight or SKIP this battle? Enter 'Fight' or 'SKIP' to choose.") ;
-  // if player choses to fight, then fight
-    if (promptFight === "fight" || promptFight === "FIGHT") {
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+var promptFight = window.prompt("Would you like to Fight or SKIP this battle? Enter 'Fight' or 'SKIP' to choose.") ;
+  
+// if player choses to fight, then fight
+  if (promptFight === "fight" || promptFight === "FIGHT") {
+  // remove enemy's health by subtracting the amount set in the playerAttack variable
+  enemyHealth = enemyHealth - playerAttack;
     console.log(
       playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
   
-    // check enemy's health
-    if (enemyHealth <= 0) {
+// check enemy's health
+  if (enemyHealth <= 0) {
       window.alert(enemyName + " has died!");
     } else {
       window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
   
-    // remove player's health by subtracting the amount set in the enemyAttack variable
+// remove player's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
       enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
@@ -61,4 +69,6 @@ var fight = function() {
     }
 }
 
-fight()
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
